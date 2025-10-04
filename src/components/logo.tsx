8de5b824541483c94f2ca574b85ada4745646fef
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-10-04 20:49:23 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative", className)}>
+    <div className={cn("relative w-8 h-8", className)}>
       <Image 
         src="https://cdn.bucket.opendex.dev/dany-carl/logo-danny-carl.webp" 
         alt="Dany Carl Logo"
-        width={15}
-        height={15}
+        fill
+        sizes="2rem"
         className="object-contain"
         priority
       />
