168dd96dffab0fb4e5b504d2a6e6786d139e67de
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-10-04 20:00:21 +0000

    el logo esta aqui    /src/app/logo-danny-carl.webp

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,12 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import logoImage from "@/app/logo-dany-carl.webp";
 
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("relative w-32 h-12", className)}>
       <Image 
-        src="/logo-danny-carl.webp" 
+        src={logoImage} 
         alt="Dany Carl Logo"
         fill
         className="object-contain"
