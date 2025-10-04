1c96299542416e8ada0441116a1a6e6b4e1d3e4b
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-10-04 20:51:17 +0000

    para que se vea tan diminuto el logo como tu lo podrias hacer (_for elem

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative w-6 h-6", className)}>
+    <div className={cn("relative w-4 h-4", className)}>
       <Image 
         src="https://cdn.bucket.opendex.dev/dany-carl/logo-danny-carl.webp" 
         alt="Dany Carl Logo"
         fill
-        sizes="1.5rem"
+        sizes="1rem"
         className="object-contain"
         priority
       />
