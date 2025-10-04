2ae4061f314cd9e57b3045cc2c365345286ea832
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-10-04 20:49:59 +0000

    ponme el logo mas pequenio esta demasiado grande (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative w-8 h-8", className)}>
+    <div className={cn("relative w-6 h-6", className)}>
       <Image 
         src="https://cdn.bucket.opendex.dev/dany-carl/logo-danny-carl.webp" 
         alt="Dany Carl Logo"
         fill
-        sizes="2rem"
+        sizes="1.5rem"
         className="object-contain"
         priority
       />
