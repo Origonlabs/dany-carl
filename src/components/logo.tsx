acdf558b7e0668c4d48892a5b584b072f44e4f6d
AGRUFINO GUZMAN GOMEZ <agrufino@cloud.opendex.dev>
2025-10-04 20:52:13 +0000

    mejor en 1

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <div className={cn("relative w-2 h-2", className)}>
+    <div className={cn("relative w-1 h-1", className)}>
       <Image 
         src="https://cdn.bucket.opendex.dev/dany-carl/logo-danny-carl.webp" 
         alt="Dany Carl Logo"
         fill
-        sizes="0.5rem"
+        sizes="0.25rem"
         className="object-contain"
         priority
       />
